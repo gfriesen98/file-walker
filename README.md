37e2c91c190dee0689adbeee3ec2fbc7a077f1ae
gfriesen98 <garettf999@gmail.com>
2021-11-07 19:08:37 -0600

    Additions
    Added -t rom: create mongo formatted output for indexing roms

--- README.md
+++ README.md
@@ -3,18 +3,44 @@
 I wanted to write a program to index my library of media using a Mongo database and I needed to be able to gather all my files and put them with their full path into the database so I wrote a script to do that for me :smiley-face:
 
 ## usage
+
 1. clone this repo
 2. `npm install`
-3. `sudo chmod u+x index.js`
-4. `./index.js -f [starting folder] >> file.json` to write out to a file.
-  - *it is preferred to use a full path like `/home/username/files_i_want/`*
-5. done import data like a boss
+3. `sudo chmod u+x index.js` or use with `node` instead
+4. `./index.js -t [movie|rom] -f [starting folder] >> file.json` to write out to a file
+5. done, import data like a boss
 
-## usage part 2
-I wrote this for use with a file structure that Plex uses, specifically for Movies. [Click here to learn more](https://support.plex.tv/articles/aming-and-organizing-your-movie-media-files/) about the kind of file structure im talking about.
+## movies
 
-The script will OMIT folders named `Featurettes` and `Subs`, as well as files ending with `.sfv` and `.srt` as i dont want those files in my database.
+I wrote this for use with a file structure that Plex uses, specifically for Movies. [Click here to learn more](https://support.plex.tv/articles/naming-and-organizing-your-tv-show-files/) about the kind of file structure im talking about.
+
+Expected output: `{"name": 'The Room', path: "/user/path/to/The Room", "fileSize": "1kb"}`
+
+The script (when `-t movie` is run) will OMIT folders named `Featurettes` and `Subs`, as well as files ending with `.sfv` and `.srt` because I dont really want those in my database.
+
+## roms
+
+Like for getting movie names, running `-t rom` searches a directory for files but grabs more relevant information like filesize and category of rom.
+
+The script will assume your folder structure is something like:
+```
+/
+  /SNES
+    rom
+  /NES
+    rom
+  /Neogeo
+    /BIOS
+      bios
+    rom
+    rom
+  ...etc
+```
+
+Expected output: `{"category": "SNES", "name": "rom", "fileSize": "1kb"}`
+
+If there are subdirectory, the category for those files in the subdirectory will be the name of that subdir: `{"category": "BIOS", "name": "bios", "fileSize": "1kb"}`
 
 ## other
 
-feel free to check out my code and change it around, its pretty small.
+if there is something missing just clone and add it urself :)
--- index.js
+++ index.js
@@ -3,10 +3,16 @@
  * This program goes through a folder given and creates a list of
  * json objects for use in Mongodb.
  * 
- * This is meant to be used in a plex media directory such as
- *  Movies
- *    - Movie (2019) => used for the name of the movie
- *      - Movie.mkv  => used for the fullpath of the media file (/Movies/Movie(2019)/Movie.mkv)
+ * Depending on -t options input:
+ * 
+ * movie:
+ *  This is meant to be used in a plex media directory such as
+ *   Movies
+ *     - Movie (2019) => used for the name of the movie
+ *       - Movie.mkv  => used for the fullpath of the media file (/Movies/Movie(2019)/Movie.mkv)
+ * 
+ * rom:
+ *   This is meant to be used a directory with roms. Seperate from movie so we can be more specific for roms and disregard any checks movie does
  */
 const args = require('yargs');
 const fs = require('fs');
@@ -14,32 +20,75 @@ const path = require('path');
 
 //set up arguments
 const options = args.usage("Usage: -f <starting folder>")
-                    .option("f", 
-                      {alias: "folder", describe: "Starting folder", type: "string", demandOption: true})
-                    .argv;
+  .option("t",
+    { alias: "type", describe: "Select type 'movie' or 'roms'", type: "string", demandOption: true })
+  .option("f",
+    { alias: "folder", describe: "Starting folder", type: "string", demandOption: true })
+  .argv;
+
+  /**
+   * Gets a human readable formatted file size
+   * @param {*} file file path
+   * @returns {string} file size
+   */
+function getFileSize(file) {
+  var fileSize = fs.statSync(file).size;
+  var i = Math.floor(Math.log(fileSize) / Math.log(1024));
+  return ((fileSize / Math.pow(1024, i).toFixed(2) * 1).toFixed(2) + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i]);
+}
+
+/**
+ * Walks through all files in a given directory, and files in subdirectories of the directory
+ * @param {string} dir starting directory
+ */
+function walkRom(dir) {
+  const array = [];
+  fs.readdirSync(dir).forEach(file => {
+    let fullpath = path.join(dir, file);
+    if (fs.lstatSync(fullpath).isDirectory()) {
+      walkRom(fullpath);
+    } else {
+      const obj = { 
+        "category": path.basename(path.dirname(fullpath)),
+        "name": (fullpath.substring(fullpath.lastIndexOf('/')+1).replace(/\.[^/.]+$/, "")).replaceAll('_', ' '),
+        "path": fullpath, 
+        "fileSize": getFileSize(fullpath)
+      };
+      array.push(obj);
+    }
+  });
+  array.forEach(n => console.log(JSON.stringify(n)));
+}
 
 /**
  * Walks through all files in a given directory, and files in subdirectories of the directory/
  * @param {*} dir starting directory
  */
-function walk(dir) {
-  var array = [];
+function walkMovie(dir) {
+  const array = [];
   fs.readdirSync(dir).forEach(file => {
     let fullpath = path.join(dir, file);
-    if (fs.lstatSync(fullpath).isDirectory() && !fullpath.match(/(Featurettes)/g) && !fullpath.match(/(Subs)/g)){
+    if (fs.lstatSync(fullpath).isDirectory() && !fullpath.match(/(Featurettes)/ig) && !fullpath.match(/(Subs)/ig)) {
       //If file is a directory, check that directory (and ignore Featurettes/Subs folders)
-      walk(fullpath);
+      walkMovie(fullpath);
     } else {
-      if (!fullpath.endsWith(".srt") && !fullpath.endsWith(".sfv") && !fullpath.match(/(Featurettes)/g) && !fullpath.match(/(Subs)/g)){
+      if (!fullpath.endsWith(".srt") && !fullpath.endsWith(".sfv") && !fullpath.match(/(Featurettes)/g) && !fullpath.match(/(Subs)/g)) {
         //Ignore .srt/.sfv/Featurettes/Subs
-        const template = {"name": path.basename(path.dirname(fullpath)), "path": fullpath};
+        const template = { 
+          "name": path.basename(path.dirname(fullpath)), 
+          "path": fullpath,
+          "fileSize": getFileSize(fullpath)
+        };
         array.push(template);
       }
     }
   });
-  
   //stdout print the results.
-  array.forEach(n => console.log(n));
+  array.forEach(n => console.log(JSON.stringify(n)));
 }
 
-walk(options.folder);
+if (options.type === 'movie') {
+  walkMovie(options.folder);
+} else if (options.type === 'rom') {
+  walkRom(options.folder);
+}
